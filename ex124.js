50637510c7faaa1c966ef668c21127e00c9e6e17
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2024-05-03 21:32:56 +0100

    Add files via upload

---
+++ ex124.js
@@ -0,0 +1,11 @@
+function remplaceChiffres(ch) {
+    var c =''
+    for (var i = 0; i < ch.length; i++) {
+        if (ch[i] == "0" || ch[i] == "1"||ch[i] == "2" || ch[i] == "3"||ch[i] == "4" || ch[i] == "5"||ch[i] == "6" || ch[i] == "7"||ch[i] == "8" || ch[i] == "9"){
+            c  += "*";
+        }else{
+            c += ch[i];
+        }
+    }
+    return c
+}
